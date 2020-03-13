33723fa24a4c7baeff40f7f22e9e0b99f253050e
wei2019wei <56536290+wei2019wei@users.noreply.github.com>
2020-03-13 14:58:48 +0800

    Add files via upload

---
+++ 后台/IDEA springboot maven 多模块/xproject/xproject-web/src/main/java/fun/chouniu/xprojectweb/XprojectWebApplication.java
@@ -0,0 +1,13 @@
+package fun.chouniu.xprojectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XprojectWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XprojectWebApplication.class, args);
+    }
+
+}
---
+++ 后台/IDEA springboot maven 多模块/xproject/xproject-web/src/test/java/fun/chouniu/xprojectweb/XprojectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.chouniu.xprojectweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XprojectWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
